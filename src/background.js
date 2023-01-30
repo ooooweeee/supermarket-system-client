3d7ad9097fe60afe71248901e86eb691686ae769
ooooweeee <ooooweeee@github.com>
2023-01-30 20:11:59 +0800

    完善数据库操作

--- src/background.js
+++ src/background.js
@@ -1,23 +1,20 @@
 'use strict';
 
-import fs from 'fs';
 import path from 'path';
 import { app, protocol, BrowserWindow } from 'electron';
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib';
-import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer';
-import sqlite3 from 'sqlite3';
+import database from './database';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
-const { Database } = sqlite3.verbose();
-const SECRET_KEY = 'supermarket_system_db';
 
-// Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ]);
 
-async function createWindow() {
-  // Create the browser window.
+app.on('ready', async () => {
+  if (!isDevelopment) {
+    createProtocol('app');
+  }
   const win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -29,77 +26,17 @@ async function createWindow() {
   });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-    if (!process.env.IS_TEST) win.webContents.openDevTools();
+    if (isDevelopment) win.webContents.openDevTools();
   } else {
-    createProtocol('app');
-    // Load the index.html when not in development
     win.loadURL('app://./index.html');
   }
-}
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS3_DEVTOOLS);
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString());
-    }
-  }
-  createWindow();
-  const dbDir = path.join(app.getPath('userData'), 'database');
-  fs.mkdirSync(dbDir, { recursive: true });
-  const db = new Database(path.join(dbDir, 'db_management.db'));
-  db.serialize(() => {
-    db.exec('PRAGMA cipher_compatibility = 3');
-    db.exec(`PRAGMA key = ${SECRET_KEY}`);
-  });
-  db.run(
-    `CREATE TABLE IF NOT EXISTS db_table (
-    employee_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    employee_name TEXT NOT NULL
-  )`,
-    err => {
-      db.run(`INSERT INTO db_table (employee_name) VALUES ('oooweee')`, err => {
-        db.get(`SELECT * FROM db_table`, (err, raw) => {
-          console.log(raw);
-        });
-      });
-    }
+  const db2 = database();
+  const db = database();
+  await db.createDatatable();
+  await db.asyncRun(
+    `INSERT INTO dh_employees (dh_employee_phone, dh_employee_password, dh_employee_name) VALUES ('${Date.now()}', '000000', 'oooweee')`
   );
+  const result = await db2.asyncAll(`SELECT * FROM dh_employees`);
+  console.log(result);
 });
-
-// Exit cleanly on request from parent process in development mode.
-if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', data => {
-      if (data === 'graceful-exit') {
-        app.quit();
-      }
-    });
-  } else {
-    process.on('SIGTERM', () => {
-      app.quit();
-    });
-  }
-}
---
+++ src/database/index.js
@@ -0,0 +1,83 @@
+import fs from 'fs';
+import path from 'path';
+import { app } from 'electron';
+import sqlite3 from 'sqlite3';
+
+const { Database } = sqlite3.verbose();
+const SECRET_KEY = 'dh_supermarket_system_db';
+const dbDir = path.join(app.getPath('userData'), 'database');
+
+class DB {
+  constructor() {
+    fs.rmdirSync(dbDir, { recursive: true });
+    fs.mkdirSync(dbDir, { recursive: true });
+    this.db = new Database(path.join(dbDir, 'db_management.db'));
+    this.db.serialize(async () => {
+      this.db.exec('PRAGMA cipher_compatibility = 3');
+      this.db.exec(`PRAGMA key = ${SECRET_KEY}`);
+    });
+  }
+
+  asyncRun(command) {
+    return new Promise((resolve, reject) => {
+      this.db.run(command, (err, res) => (err ? reject(err) : resolve(res)));
+    });
+  }
+  asyncGet(command) {
+    return new Promise((resolve, reject) => {
+      this.db.get(command, (err, res) => (err ? reject(err) : resolve(res)));
+    });
+  }
+  asyncAll(command) {
+    return new Promise((resolve, reject) => {
+      this.db.all(command, (err, res) => (err ? reject(err) : resolve(res)));
+    });
+  }
+
+  static getInstance() {
+    if (!DB.instance) {
+      DB.instance = new DB();
+    }
+    return DB.instance;
+  }
+
+  createDatatable() {
+    return Promise.all([
+      this.asyncRun(`CREATE TABLE IF NOT EXISTS dh_employees (
+        dh_employee_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        dh_employee_phone TEXT NOT NULL UNIQUE,
+        dh_employee_password TEXT NOT NULL,
+        dh_employee_name TEXT NOT NULL,
+        dh_employee_sex INTEGER DEFAULT 0,
+        dh_employee_address TEXT,
+        dh_employee_state INTEGER DEFAULT 0,
+        dh_employee_update_date TEXT DEFAULT (DATETIME(CURRENT_TIMESTAMP, 'localtime'))
+        )`),
+      this.asyncRun(`CREATE TABLE IF NOT EXISTS dh_categories (
+        dh_category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        dh_category_name TEXT NOT NULL UNIQUE,
+        dh_category_state INTEGER DEFAULT 0,
+        dh_category_update_date TEXT DEFAULT (DATETIME(CURRENT_TIMESTAMP, 'localtime'))
+      )`),
+      this.asyncRun(`CREATE TABLE IF NOT EXISTS dh_goods (
+        dh_goods_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        dh_goods_name TEXT NOT NULL,
+        dh_goods_price INTEGER NOT NULL,
+        dh_goods_sale_price INTEGER NOT NULL,
+        dh_goods_category_id INTEGER NOT NULL,
+        dh_goods_store INTEGER NOT NULL,
+        dh_goods_state INTEGER NOT NULL,
+        dh_goods_update_date TEXT DEFAULT (DATETIME(CURRENT_TIMESTAMP, 'localtime'))
+      )`),
+      this.asyncRun(`CREATE TABLE IF NOT EXISTS dh_incidents (
+        dh_incident_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        dh_incident_action INTEGER NOT NULL,
+        dh_incident_goods_id INTEGER NOT NULL,
+        dh_incident_sale_num INTEGER NOT NULL,
+        dh_incident_update_date TEXT DEFAULT (DATETIME(CURRENT_TIMESTAMP, 'localtime'))
+      )`)
+    ]);
+  }
+}
+
+export default DB.getInstance;
